7e58b99019c7d6800d7fdb1f218934e8a7a4bfdb
Memo Moran <tuahil@gmail.com>
2021-02-21 20:00:28 -0700

    Se agregaron mas ejemplos a reduce

--- filter.js
+++ filter.js
@@ -46,3 +46,30 @@
 // });
 
 // console.log(isUnique);
+
+
+//EXAMPLE 4 - Filtrar un array de objetos por alguna propiedad espeficifica
+
+// const json = {
+//     'homes':[ 
+//      {
+//          "home_id":"1",
+//          "price": "925",
+//          "sqft":"1100",
+//          "num_of_beds":"2",
+//          "num_of_baths":"2.0"
+//      },
+//      {
+//         "home_id":"2",
+//         "price": "1425",
+//         "sqft":"1900",
+//         "num_of_beds":"4",
+//         "num_of_baths":"2.5"
+//     },
+// ]
+// };
+
+// const homes = json.homes;
+
+
+// console.log(homes.filter(home => home.price === '925' ));
--- reduce.js
+++ reduce.js
@@ -140,7 +140,7 @@
     
 // },0);
 
-
+// console.log(res);
 
 //EJEMPLO 8
 // We nees an array [ first name, last name] of all users only if:
@@ -199,7 +199,7 @@
 //     {name:'apples', quantity: 2, id:0},
 //     {name:'bananas', quantity: 0, id:1},
 //     {name:'cherries', quantity: 5, id:2},
-//     //{name:'bananas', quantity: 0, id:3},
+//     {name:'bananas', quantity: 0, id:3},
 // ];
 
 // const thisShitIsBananas = fruits.reduce((acc, fruit) => {
@@ -219,6 +219,143 @@
 // const numbers = [5,4,3,2,1];
 // const factorial = numbers.reduce((acc,number)=> acc += number);
 // console.log(factorial);
+
+
+//EJEMPLO 11 - Filtrar individuis por ciertas condiciones
+
+/*
+Estudiantes de una universidad de cuyo nombre no quiero acordarme, realizo un estudio para determinar
+"cuanto pesa ir a la escuela". El estudio consistio en determinar si el peso que carga un estudiante es su bolso cuando
+va a la escuela puede influir en su desempe;o, !y que creen?, encontraros que si un estudiante carga mas de 2 kilos en su 
+bolso, este tiende a tener mal desempe;o.
+
+Segun estre estudio identificar quienes de los sigueintes alumnos podrian desempe;arse mal en el estudio, suponiendo
+que los siguientes elementos pesan lo mismo en cada uno de sus bolsos:
+
+Cuaderno: 0.2 kilos
+Cartilla: 0.6 kilos
+Trabajo de ciencias: 0.8 kilos
+Lapiz: 0.1 kilos
+Lonchera: 1 kilo (Comen mucho) :v
+*/
+
+//Datos de ejercisio
+const alumnos = [
+  {
+    nombre: 'Oscar',
+    bolso: ['Cuaderno','Cuaderno','Cuaderno','Cuaderno','Lapiz','Lapiz']
+  },
+  {
+    nombre: 'Maria',
+    bolso: ['Cuaderno','Cuaderno','Cartilla','Lapiz','Lapiz']
+  },
+  {
+    nombre: 'Miguel',
+    bolso: ['Cartilla','Cartilla','Lapiz','Lonchera']
+  },
+  {
+    nombre: 'Sandra',
+    bolso: ['Cuaderno','Lapiz','Lonchera']
+  },
+  {
+    nombre: 'David',
+    bolso: ['Cuaderno','Trabajo de ciencias','Lapiz']
+  },
+  {
+    nombre: 'Tatiana',
+    bolso: ['Trabajo de ciencias','Lapiz']
+  },
+  {
+    nombre: 'Fernando',
+    bolso: ['Lapiz','Lonchera','Lonchera'],
+  }
+];
+
+
+
+
+// const malDesempeno = alumnos.reduce((acc,alumno) => {
+
+    // const cuaderno = 0.2;
+    // const cartilla = 0.6; 
+    // const trabajoDeCiencias = 0.8;
+    // const lapiz =  0.1;
+    // const lonchera = 1;
+    // const bolsoAlumno = alumno.bolso;
+    
+//      const pesoEnBolsa = bolsoAlumno.reduce((acc,item) => {
+ 
+//       if(item === 'Cuaderno'){
+//          acc+=cuaderno;
+//      }
+//      if(item === 'Cartilla'){
+//          acc+= cartilla;
+//      }
+//      if(item === 'Trabajo de ciencias'){
+//         acc+= trabajoDeCiencias;
+//      }
+//      if(item === 'Lapiz'){
+//         acc+= lapiz;
+//      }
+     
+//      if(item === 'Lonchera'){
+//       acc+= lonchera;
+//      }
+//      return acc;
+     
+//     },0);
+
+
+//     if(pesoEnBolsa > 2){
+//       return acc += alumno.nombre + ',';
+//     }
+//     return acc;
+//      },[]);
+
+//    console.log(malDesempeno);
+
+
+
+//Different approach utilizing for of array
+
+//     const cuaderno = 0.2;
+//     const cartilla = 0.6; 
+//     const trabajoDeCiencias = 0.8;
+//     const lapiz =  0.1;
+//     const lonchera = 1;
+  
+
+// const malDesempeno = alumnos.reduce((acc,alumno)=>{
+//      let pesoEnBolsa = 0;
+//      for(const item of alumno.bolso ) {
+//       if(item === 'Cuaderno'){
+//           pesoEnBolsa += cuaderno;
+//         }
+//         if(item === 'Cartilla'){
+//           pesoEnBolsa += cartilla;
+//         }
+//         if(item === 'Trabajo de ciencias'){
+//           pesoEnBolsa += trabajoDeCiencias;
+//         }
+//         if(item === 'Lapiz'){
+//           pesoEnBolsa += lapiz;
+//         }
+        
+//         if(item === 'Lonchera'){
+//           pesoEnBolsa += lonchera;
+//         }
+//      }
+//      if(pesoEnBolsa > 2) acc.push(alumno.nombre);
+//      return acc;
+// },[]);
+   
+
+// console.log(malDesempeno);
+ 
+ 
+      
+  
+
 
 
 
